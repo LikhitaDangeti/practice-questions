ca6e4f06491255d97c259a017b9663e1e16a6f20
Nymos <122101271+LikhitaDangeti@users.noreply.github.com>
2024-12-22 18:51:51 +0530

    remove unneccesary function

--- filter.js
+++ filter.js
@@ -27,10 +27,6 @@ const filterActiveUsers = function (users) {
 };
 
 // numbers greater than 10 [5, 12, 7, 18, 3] => [12, 18]
-const greaterThan10 = function (number) {
-  return number > 10;
-};
-
 const filterNumbersGreaterThanTen = function (numbers) {
   return numbers.filter(function (number) {
     return number > 10;
