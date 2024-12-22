2ae99906a963144ffbb8e733b0d4929076207c66
Nymos <122101271+LikhitaDangeti@users.noreply.github.com>
2024-12-22 14:07:45 +0530

    added reversedWordsOf Function

--- map.js
+++ map.js
@@ -173,7 +173,9 @@ const cumulativeSumsOf = function (arrays) {
 };
 
 // reverse words in ["hello world", "goodbye moon"] => ["olleh dlrow", "eybdoog noom"]
-const reversedWordsOf = function (strings) { };
+const reversedWordsOf = function (strings) { 
+  return strings.map(reverseString);
+};
 
 // extract unique characters from ["apple", "banana", "grape"] => ["apl", "ban", "gra"]
 // Maintain the order of their first appearance in each string
