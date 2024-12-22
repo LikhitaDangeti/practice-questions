9bdbc47d646c1af88f00db9148f65237fd824bcf
Nymos <122101271+LikhitaDangeti@users.noreply.github.com>
2024-12-22 18:11:45 +0530

    added filterLongWords Function

--- filter.js
+++ filter.js
@@ -8,7 +8,13 @@ const filterEvenNumbers = function (numbers) {
 };
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
-const filterLongWords = function (words) { };
+const longerThanFive = function (word) {
+  return word.length > 5;
+};
+
+const filterLongWords = function (words) { 
+  return words.filter(longerThanFive);
+};
 
 // people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
 const filterAdults = function (people) { };
